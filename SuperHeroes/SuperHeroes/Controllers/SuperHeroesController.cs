24aea40508bce067a4047bb7a694ef265079b3b6
Josi <JosiGrc@users.noreply.github.com>
2019-08-29 14:49:07 -0500

    made index and db and some links

---
+++ SuperHeroes/SuperHeroes/Controllers/SuperHeroesController.cs
@@ -0,0 +1,105 @@
+﻿using SuperHeroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes.Controllers
+{
+    public class SuperHeroesController : Controller
+    {
+        //Variables
+        ApplicationDbContext context;
+
+        //Ctor//////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public SuperHeroesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        //Methods///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // GET: SuperHeroes
+        public ActionResult Index(SuperHeroe superheroe)
+        {
+            //bring the table of hereoes here
+            return View();
+        }
+
+        // GET: SuperHeroes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperHeroes/Create
+        public ActionResult Create()
+        {
+            SuperHeroe superheroe = new SuperHeroe();
+            return View(superheroe);
+        }
+
+        // POST: SuperHeroes/Create
+        [HttpPost]
+        public ActionResult Create(SuperHeroe superheroe)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                context.SuperHeroes.Add(superheroe);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SuperHeroes/SuperHeroes/Models/SuperHeroe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHeroe
+    {
+        //Variables
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimarySuperHeroAbility { get; set; }
+        public string SeocondarySuperHeroeAbility { get; set; }
+        public string Catchphrase { get; set; }
+        //Ctor
+
+        //Methods
+    }
+}
